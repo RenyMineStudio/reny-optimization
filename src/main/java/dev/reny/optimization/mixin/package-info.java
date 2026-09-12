4358278d8f4a11020973ea2ee4798b40cd029aa3
pedroteste00000008-stack <pedroteste00000008@gmail.com>
2026-09-11 20:47:41 -0400

    build: establish empty mixin package

---
+++ src/main/java/dev/reny/optimization/mixin/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Mixin namespace reserved for Reny runtime patches.
+ *
+ * <p>The package intentionally contains no active mixins in the bootstrap milestone.
+ */
+package dev.reny.optimization.mixin;
